33a11089315961e0e7f4aa46cb59c159e1173fd4
Archit Jain <146457258+JainArchit16@users.noreply.github.com>
2026-01-20 15:44:10 +0530

    Add secure option to mail transporter configuration

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -9,6 +9,7 @@ const mailSender = async (email, title, body) => {
     let transporter = nodeMailer.createTransport({
       host: process.env.MAIL_HOST,
       port: 587,
+      secure: false,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
