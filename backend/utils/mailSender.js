366334ec8d9e9909ef96105d7208f33ce8e1be44
Archit Jain <146457258+JainArchit16@users.noreply.github.com>
2026-01-20 15:50:49 +0530

    Add TLS option to mailSender for debugging
    Added TLS option to mail transporter for debugging.

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -14,6 +14,9 @@ const mailSender = async (email, title, body) => {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
+      tls: {
+        rejectUnauthorized: false, // TEMP for debugging
+      },
     });
     let info = await transporter.sendMail({
       from: "PortFolio Website-Archit Jain",
